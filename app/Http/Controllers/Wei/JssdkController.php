4d2db41ae9495e08def9b51a6c6d2d16095924e4
Your Name <lianshijie@3023668879.com>
2019-04-24 17:49:54 +0800

    debug

--- app/Http/Controllers/Wei/JssdkController.php
+++ app/Http/Controllers/Wei/JssdkController.php
@@ -50,12 +50,12 @@ class JssdkController extends Controller
         $code=$_GET['code'];
         //通过code换取网页授权access_token
         $url='https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('WX_APPID').'&secret='.env('WX_APPSECRET').'&code='.$code.'&grant_type=authorization_code';
-        $response=json_decode(file_get_content($url),true);
+        $response=json_decode(file_get_contents($url),true);
         $access_token=$response['access_token'];
         $openid=$response['openid'];
         //拉取用户信息
         $urla='https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
-        $res=json_decode(file_get_content($urla),true);
+        $res=json_decode(file_get_contents($urla),true);
         echo'<pre>';print_r($re);echo'</pre>';
     }
 }
