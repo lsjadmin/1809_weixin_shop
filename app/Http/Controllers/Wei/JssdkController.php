dba119a523fde36e24da71b7ba484e25e887cf11
Your Name <lianshijie@3023668879.com>
2019-04-24 19:09:11 +0800

    debug

--- app/Http/Controllers/Wei/JssdkController.php
+++ app/Http/Controllers/Wei/JssdkController.php
@@ -52,22 +52,29 @@ class JssdkController extends Controller
         //通过code换取网页授权access_token
         $url='https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('WX_APPID').'&secret='.env('WX_APPSECRET').'&code='.$code.'&grant_type=authorization_code';
         $response=json_decode(file_get_contents($url),true);
-        echo'<pre>';print_r($response);echo'</pre>'; 
+        //echo'<pre>';print_r($response);echo'</pre>'; 
 
         $access_token=$response['access_token'];
         $openid=$response['openid'];
         //拉取用户信息
         $urla='https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
         $res=json_decode(file_get_contents($urla),true);
-        echo'<pre>';print_r($res);echo'</pre>';die;
-       
-            $info=[
-                'openid'=>$res['openid'],
-                'nickname'=>$res['nickname'],
-                'city'=>$res['city'],
-                'province'=>$res['province'],
-                'country'=>$res['country'],
-            ];
-            $arr=MessageModel::insert($info);
+        //echo'<pre>';print_r($res);echo'</pre>';die;
+        
+        $name=MessageModel::where(['openid'=>$res['openid']])->first();
+            if($name==$res['nickname']){
+                echo "欢迎回来";
+            }else{
+                echo '欢迎您'.$res['nickname'];
+                $info=[
+                    'openid'=>$res['openid'],
+                    'nickname'=>$res['nickname'],
+                    'city'=>$res['city'],
+                    'province'=>$res['province'],
+                    'country'=>$res['country'],
+                ];
+                $arr=MessageModel::insert($info);
+            }
+            
     }
 }
