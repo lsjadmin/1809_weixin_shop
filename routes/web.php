18236ebc7526fdc4785196ee8dc31004de718920
Your Name <lianshijie@3023668879.com>
2019-04-25 19:23:14 +0800

    debug

--- routes/web.php
+++ routes/web.php
@@ -51,17 +51,5 @@ Route::get('valid','Wei\WeiController@valid');
 Route::any('valid','Wei\WeiController@wxEvent');
 Route::get('success_toke','Wei\WeiController@success_toke');
 Route::get('test','Wei\WeiController@test');
-//微信创建菜单
-Route::any('createMenu','Wei\WeiController@createMenu');
-//测试
-Route::get('a','Wei\WeiController@a');
-//群发 
-Route::get('send','Wei\WeiController@send');
-//微信支付
-Route::get('text','Wei\WeiPayController@text');
-Route::post('notify','Wei\WeiPayController@notify');
-//回复图文消息
-Route::any('desc/{id}','Wei\WeiController@desc');
-Route::any('cong','Wei\WeiController@cong');
 
 
