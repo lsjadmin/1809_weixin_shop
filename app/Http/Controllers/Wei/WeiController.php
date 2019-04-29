7eac59bbea008bc3512c4336d4f59ff6e626f30a
Your Name <lianshijie@3023668879.com>
2019-04-29 18:59:49 +0800

    debug

--- app/Http/Controllers/Wei/WeiController.php
+++ app/Http/Controllers/Wei/WeiController.php
@@ -253,7 +253,7 @@ class WeiController extends Controller
     }
 
     public function scopea(){
-      echo'<pre>';print_r($_GET);echo'</pre>';die;
+      //echo'<pre>';print_r($_GET);echo'</pre>';die;
        $code=$_GET['code'];
        //通过code换取网页授权access_token
        $url='https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('WX_APPID').'&secret='.env('WX_APPSECRET').'&code='.$code.'&grant_type=authorization_code';
@@ -278,6 +278,11 @@ class WeiController extends Controller
                    'country'=>$res['country'],
                ];
                $arr=MessageModel::insert($info);
+               if($arr){
+                echo "ok";
+               }else{
+                echo "no";
+               }
            }
            
    }
